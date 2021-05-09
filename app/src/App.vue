37ac2b783719e0ae31abf8b23d3ee3f8b9c25b57
chenhung lin <>
2021-05-10 00:43:27 +0800

    fix css style in app/src/App.vue

--- app/src/App.vue
+++ app/src/App.vue
@@ -55,13 +55,16 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import './assets/styles/_reset.scss';
+@import './assets/styles/variables.scss';
+@import "./assets/styles/global.scss";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
